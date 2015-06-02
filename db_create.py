22faba669238e1f53ccdc6f190d7b421a218f65f
SleipRecx <markus.andresen94@gmail.com>
2015-06-02 18:46:52 +0200

    fuck

--- db_create.py
+++ db_create.py
@@ -7,7 +7,9 @@ db.create_all()
 #insert
 db.session.add(BlogPost("good","i am good."))
 db.session.add(BlogPost("well","i am well."))
+db.session.add(BlogPost("postgres","we setup a local postgres"))
 
 
 #commit changes
 db.session.commit()
+
